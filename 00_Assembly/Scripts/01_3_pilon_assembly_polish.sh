4b1f0a9ae3196a336d546e8262f2de12523079db
mmontonerin <merce.montoliu@gmail.com>
2022-09-26 16:07:16 +0200

    Update on pilon commands

--- 00_Assembly/Scripts/01_3_pilon_assembly_polish.sh
+++ 00_Assembly/Scripts/01_3_pilon_assembly_polish.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-for flyline in "MS" "O11" A28" "D_insularis" "D_paulistorum_L06" "D_paulistorum_L12" "D_sp" "D_sucinea" "D_tropicalis" "D_willistoni_00" "D_willistoni_LG3"
+for flyline in "A28" "MS" "O11" "D_equinoxialis" "D_insularis" "D_paulistorum_L06" "D_paulistorum_L12" "D_sp" "D_sucinea" "D_tropicalis" "D_willistoni_00" "D_willistoni_LG3"
 do
         assembly="./${flyline}_pepper_cor.fasta"
-        
-	#D.paulistorum L12 lacks Illumina data, so we used the closest semi-species data, D.paulistorum MS
-	if ${flyline} =~ "D_paulistorum_L12"
+
+        #D_paulistorum_L12 does not have Illumina data, and thus we use the phylogenetically closest semi-species, MS:
+        if ${flyline} =~ "D_paulistorum_L12"
         then
-                illumina1="D_paulistorum_MS_R1.fastq.gz"
-                illumina2="D_paulistorum_MS_R2.fastq.gz"
+                illumina1="../paulistorum/MS_illumina_R1.fastq.gz"
+                illumina2="../paulistorum/MS_illumina_R2.fastq.gz"
         else
-        illumina1="${flyline}_illumina_R1.fastq.gz"
-        illumina2="${flyline}_illumina_R2.fastq.gz"
+        illumina1="../paulistorum/${flyline}_illumina_R1.fastq.gz"
+        illumina2="../paulistorum/${flyline}_illumina_R2.fastq.gz"
         fi
-	
-	#Check that it is running the right assembly with the right reads
+
         echo "Running:"
-	echo "$assembly"
+        echo "$assembly"
         echo "$illumina1"
         echo "$illumina2"
 
+        echo "Indexing assembly"
         #Index assembly
         /space/Software/bwa/bwa index $assembly
 
+        echo "mapping illumina reads"
         #Map Illumina reads to the pepper-polished assembly
         /space/Software/bwa/bwa mem -t 20 $assembly $illumina1 $illumina2 | samtools view -bSu - | samtools sort -@ 20 -o ${flyline}_map.bam
 
         #Index BAM files
         samtools index ${flyline}_map.bam
 
+        echo "Running Pilon"
         java -Xmx120G -jar /space/Software/pilon/pilon-1.24.jar --genome $assembly --bam ${flyline}_map.bam --outdir ${flyline}_pilon \
                 --changes --tracks --vcf --fix "indels" --mindepth 10
-        
+
 
 
 done
